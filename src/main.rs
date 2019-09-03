9a5a0aa738a893d6a3434b06f9bc3217e59f40d5
lamartire <lamartire@gmail.com>
2019-09-03 13:13:50 +0300

    feat: move home dir util to external function

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,36 @@ use std::env;
 use std::fs;
 use std::path;
 
-fn copy_snippet(snippet_path: path::PathBuf) {
-    let mut new_path_parts: Vec<String> = Vec::new();
+fn get_home_dir_path() -> String {
+    let mut home_path_parts: Vec<String> = Vec::new();
     let src_path = path::PathBuf::from("./src");
 
-    for src_path_part in fs::canonicalize(&src_path).iter() {
-        for entry in src_path_part.to_string_lossy().split("/") {
-            if entry != "" && new_path_parts.len() < 2 {
-                new_path_parts.push(entry.to_string());
+    for home_path_part in fs::canonicalize(&src_path).iter() {
+        for entry in home_path_part.to_string_lossy().split("/") {
+            if entry != "" && home_path_parts.len() < 2 {
+                home_path_parts.push(entry.to_string());
             }
         }
     }
 
+    home_path_parts.join("/")
+}
+
+fn copy_snippet(snippet_path: path::PathBuf) {
+    let new_path_parts: Vec<String> = Vec::new();
     let mut new_path = path::PathBuf::from("/");
     let mut snippet_name_parts: Vec<String> = Vec::new();
 
     for path_part in snippet_path.iter() {
         let path_part_str = path_part.to_string_lossy().to_string();
 
-        if path_part_str != "snippets" {
+        if path_part_str != "sublime-text-3" && path_part_str != "snippets" {
             snippet_name_parts.push(path_part_str);
         }
     }
 
     new_path.push(new_path_parts.join("/"));
+    new_path.push(get_home_dir_path());
     new_path.push("Library/Application Support/Sublime Text 3/Packages/User");
     new_path.push(snippet_name_parts.join("-"));
 
@@ -34,7 +40,9 @@ fn copy_snippet(snippet_path: path::PathBuf) {
 }
 
 fn install_snippets() {
-    for entry in glob("./sublime-text-3/snippets/**/*.sublime-snippet").expect("Failed to install snippets!") {
+    for entry in
+        glob("./sublime-text-3/snippets/**/*.sublime-snippet").expect("Failed to install snippets!")
+    {
         match entry {
             Ok(snippet_path) => copy_snippet(snippet_path),
             Err(e) => println!("{:?}", e),
